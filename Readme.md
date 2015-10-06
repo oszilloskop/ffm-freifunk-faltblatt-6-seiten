5defbd532e171d43464fcb8d9e954898568fd10f
JuS <hermann@Jason-Mac-mini.fritz.box>
2015-10-06 22:27:02 +0200

    Initial Revision

---
+++ Readme.md
@@ -0,0 +1,63 @@
+# Freifunk Faltblatt-6-Seiten - Wickelfalz, Bund links
+
+Geschlossen 9,8 x 9,8 cm  
+Offen: 29,4 x 9,8 cm (ohne Anschnitt)  
+
+Unsymmetrie (passend für Flyeralarm) wegen Wickelfalz - Bund links   
+Dokumentseite 1: | 9,7 cm | 9,8cm | 9,9 cm |  
+Dokumentseite 2: |9,9 cm | 9,8 cm | 9,7 cm |  
+Anschnitt bis zu 2mm links/rechts/oben/unten vorbereitet
+
+
+![enter image description here](https://cloud.githubusercontent.com/assets/1434390/10319322/fc054e4e-6c6d-11e5-9439-492a63543a06.png)
+
+![enter image description here](https://cloud.githubusercontent.com/assets/1434390/10319332/01ff33b4-6c6e-11e5-953b-ebbb948c1a5e.png)
+
+# Motivation
+Das Design wurde neu Aufgesetzt.
+Texte und Grafiken wurde derart überarbeitet, dass unbedarfte DAUs (ohne jegliche Freifunk Kenntnis) nicht mit negativ vorbelasteten Begriffen konfrontiert werden.  
+# Anpassung an eure Community per PHP-Skript
+Dieses Faltblatt wurde für die Community Frankfurt erstellt.  
+**Das Faltblatt kann jedoch mittels eines beiliegenden PHP-Scripts sehr leicht an jede Community angepasst werden.**  
+
+Im ordner './lokalisierung' befindet sich das Skript 'flyermod.php'.  
+Hier einfach die Inhalte folgende Variablen an eure Community anpassen:
+
+$communityNameText  
+$kontaktTitelText  
+$kontaktInfoTexte  
+$kontaktFusszeileText  
+$kontaktLogoDateiName  
+$kontaktLogoWidth  
+$kontaktLogoPositionY  
+ggf. $communityNameFontSize  
+Die Skript-Kommentare sollten Euch eigentlich helfen (hoffentlich :o)
+
+Dann direkt im Verzeichnis das Skript aufrufen mit:
+
+php -f flyermod.php  
+Folgende Dateien sollten dann erzeugt werden:
+
+lokalisiert-faltblatt_6_seiten_9.8x9.8_ohne_anschnitt.pdf
+lokalisiert-faltblatt_6_seiten_9.8x9.8_1mm_anschnitt.pdf
+lokalisiert-faltblatt_6_seiten_9.8x9.8_2mm_anschnitt.pdf 
+
+Ggf. können mit "Gimp" oder z.B. dem Kommando "convert" (ImageMagick) die erzeugten PDFs in Grafiken umwandelt werden.
+
+# Anpassung mit Scribus 1.5.x  
+Dieses Faltblatt wurde mit **[Scribus](http://www.scribus.net/) 1.5** erstellt (hier OS X 1.5.0).
+Das Dokument heißt './faltblatt-6-seiten_9.8x9.8cm/FF_Faltblatt_6_Seiten_9.8x9.8.sla'.   
+Für die Lokalisierung kann man sich an der Ebene "Community Template" orientieren.  
+
+Das Dokument ist für einen Anschnitt auf bis zu 2mm links/rechts/oben/unten vorbereitet. Dieses ist beim PDF-Export zu beachten und ggf. anzugeben.
+
+Wird das Faltblatt per Scribus als Grafik exportiert, so geschieht dieses ohne Anschnitt.  
+
+# Basis
+Grundlage dieses hier vorliegenden Faltblattes ist eine Kieler Version.
+Siehe https://github.com/rubo77/freifunk-faltblatt-6-seiten/tree/freifunk.net
+
+# Lizenz
+Die Inhalte sind in der Regel unter CC-BY-SA veröffentlicht. In Ausnahmen ist dies explizit angegeben!
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
---
+++ lokalisierung/flyermod.php
@@ -0,0 +1,240 @@
+<?php
+
+/*
+ * All rights reserved. No warranty, explicit or implicit, provided.
+ */
+
+set_include_path(get_include_path() . PATH_SEPARATOR . './');
+require_once('fpdf/fpdf.php');
+require_once('fpdi/fpdi.php');
+
+define('FPDF_FONTPATH', './fonts');
+
+
+$templateAnschnitt_0mm = "faltblatt_6_seiten_9.8x9.8_ohne_anschnitt.pdf";
+$templateAnschnitt_1mm = "faltblatt_6_seiten_9.8x9.8_1mm_anschnitt.pdf";
+$templateAnschnitt_2mm = "faltblatt_6_seiten_9.8x9.8_2mm_anschnitt.pdf"; 
+
+if (file_exists ($templateAnschnitt_0mm)) {
+    FlyerRendering ("$templateAnschnitt_0mm", "lokalisiert", 0.0);
+} else {
+    echo "\nDie Datei $templateAnschnitt_0mm ist nicht vorhanden!\n";
+}
+
+if (file_exists ($templateAnschnitt_1mm)) {
+    FlyerRendering ("$templateAnschnitt_1mm", "lokalisiert", 1.0);
+} else {
+    echo "\nDie Datei $templateAnschnitt_1mm ist nicht vorhanden!\n";
+}
+
+if (file_exists ($templateAnschnitt_2mm)) {
+    FlyerRendering ("$templateAnschnitt_2mm", "lokalisiert", 2.0);
+} else {
+    echo "\nDie Datei $templateAnschnitt_2mm ist nicht vorhanden!\n";
+}
+
+echo "\n";
+echo "Fertig!\n";
+echo "\n";
+exit ; 
+
+function FlyerRendering ($inputFile, $outputPostfix, $anschnitt)
+{
+
+/*------------------------------------------------------------------------------------
+ * Bitte tragt hier eure lokalen Freifunk Daten ein.
+ *------------------------------------------------------------------------------------
+ *
+ * Hinweiss Community-Logo: 
+ * Moegliche Format sind GIF, JPG und PNG.
+ *
+ * Laenge des Community Namen:
+ * Falls der Community-Name zu lang ist und es zu einem Zeilenumbruch kommt,
+ * dann sollte $communityNameFontSize verkleiner werden
+ *
+-------------------------------------------------------------------------------------*/
+
+
+    // Community Name fuer Hauptseite
+    $communityNameText = "Freifunk Duckburg";
+    $communityNameFontSize = 48.0;        //in pt
+    $communityNamePositionOffsetX = 0.0;  // +/- in mm
+    $communityNamePositionOffsetY = 0.0;  // +/- in mm
+
+    // Logo auf Kontaktseite
+    $kontaktLogoDateiName = "logo-template.png";
+    $kontaktLogoWidth=66.25;    //in mm  // Muss kleiner 98.0 mm sein! Hier bitte die Breite des Lo$
+    $kontaktLogoPositionY=47.0; //in mm  // Die Hoeheneinstellung ist etwas frickelig. Es klappt ab$
+
+    // Texte fuer Seite mit Kontaktdaten
+    $kontaktTitelText = "Kontakt";
+
+    $kontaktInfoTexte = [
+                        [ "Webseite" , "http://ffdb.freifunk.net"],
+                        [ "Mail" , "info@ff-duckburg.ffdb"],
+                        [ "Mailingliste" , "subscribe@ff-duckburg.ffdb"],
+                        [ "Twitter" , "@FreiFunkDB"],
+                        [ "Treffen" , "Jeden zweiten Montag"],
+                        [ "" , "Und wo? Siehe unsere Webseite"],
+                        [ "" , ""],
+                        [ "" , ""]
+                        ];
+
+    // Text Fusszeile
+    $kontaktFusszeileText ="Freifunk Duckburg e.V.";
+
+
+
+
+/*-----------------------------------------------------------------------------------
+ *
+ * Ab hier sollte nichts mehr geander werden!
+ *
+ ------------------------------------------------------------------------------------*/
+
+    // Breite der einzelnen Seiten
+    $wRechts = 99.0; //mm
+    $wMitte  = 98.0; //mm
+    $wLinks  = 97.0; //mm
+
+    // Community Name
+    $communityNamePositionX = $wLinks + $wMitte + $communityNamePositionOffsetX; //mm
+    $communityNamePositionY = 12.2 + $communityNamePositionOffsetY; //mm
+
+    // Kontakt Titel
+    $kontaktTitelPositionX = $wLinks + 3.975; //mm
+    $kontaktTitelPositionY = 10.425; //mm
+    $kontaktTitelFontSize = 15.0; //pt
+
+    // Kontakt Info
+    $kontaktInfoTextPositionX = $kontaktTitelPositionX + 25.0; //mm
+    $kontaktInfoPositionY = $kontaktTitelPositionY + 7.0; //mm
+    $kontaktInfoZeilenOffsetY = 4.7; //mm
+    $kontaktInfoFontSize = 10.9; //pt
+
+    // Kontakt Logo
+    $kontaktLogoPositionX = $wLinks + $wMitte/2 - $kontaktLogoWidth/2;
+
+    // Kontakt Footer
+    $kontaktFooterPositionX = $wLinks; //mm
+    $kontaktFooterPositionY = 95.4; //mm
+    $kontaktFooterWidth = $wMitte; //mm
+    $kontaktFooterFontSize = 10.9; //pt
+
+
+    echo "\n";
+
+    // Output-Dasteiname zusammenbauen
+    $outputFile = $outputPostfix . "-" . $inputFile;
+
+    // initiate FPDI
+    $pdf = new FPDI();
+    echo "Input: $inputFile\n";
+    $pageCount = $pdf->setSourceFile($inputFile);
+
+    // Importiere Vorder- und Rueckseite
+    $Vorderseite = $pdf->ImportPage(1);
+    $Rueckseite = $pdf->ImportPage(2);
+    // Seitenabmessungen holen
+    $size = $pdf->getTemplateSize($Vorderseite);
+    $dokumentBreite = round($size['w'],2);
+    $dokumentHoehe = round($size['h'],2);
+    echo "Dokumenten Breite: $dokumentBreite mm\n";
+    echo "Dokumenten Hoehe: $dokumentHoehe mm\n";
+    echo "Anschnitt: $anschnitt mm\n";
+    // Vorderseite uebernehmen
+    // Anfang eines bloeden Hacks wegen des FooterZeilen-Textes.
+    // Der Footertext laesst sich nur einfügen, wenn die Seite eine A4 Seite ist.
+    // Keine Ahnung warum!
+    $pdf->AddPage('L');
+    $tplVorderseite = $pdf->importPage(1);
+    $pdf->useTemplate($tplVorderseite);
+
+    //Margin ist wegen der Rand-Platzierung des Community Names wichtig.
+    $pdf->SetMargins(0,0,0);
+
+    // erstmal alle Fonts laden
+    echo "Lade Fonts...\n";
+    $pdf->AddFont('lato-bold');
+    $pdf->AddFont('lato-regular');
+    $pdf->AddFont('alternategothic');
+
+    // Rendern Titel Text
+    echo "Verarbeite Titel Text...\n";
+    $pdf->SetFont('lato-bold');
+    $pdf->SetFontSize($kontaktTitelFontSize);
+    $pdf->SetTextColor(0,0,0); //schwarz
+    $pdf->SetXY($kontaktTitelPositionX + $anschnitt, $kontaktTitelPositionY + $anschnitt);
+    $pdf->Write(0,$kontaktTitelText);
+
+    // Rendern Info Text
+    echo "Verarbeite Info Text...\n";
+    $pdf->SetTextColor(0,0,0); //schwarz
+    $pdf->SetFontSize($kontaktInfoFontSize);
+    foreach ($kontaktInfoTexte as $a) {
+        	$pdf->SetFont('lato-bold');
+        	$pdf->SetXY($kontaktTitelPositionX + $anschnitt, $kontaktInfoPositionY + $anschnitt);
+        	$pdf->Write(0,$a[0]);
+        	$pdf->SetFont('lato-regular');
+        	$pdf->SetXY($kontaktInfoTextPositionX + $anschnitt, $kontaktInfoPositionY +$anschnitt);
+        	$pdf->Write(0,$a[1]);
+        	$kontaktInfoPositionY=$kontaktInfoPositionY + $kontaktInfoZeilenOffsetY;
+    }
+
+    // Rendern Community Logo
+    echo "Verarbeite Logo...\n";
+    $pdf->Image($kontaktLogoDateiName,$kontaktLogoPositionX + $anschnitt, $kontaktLogoPositionY + $anschnitt, $kontaktLogoWidth,0);
+
+    // Rendern Fusszeilen Text
+    echo "Verarbeite Fusszeile...\n";
+    $pdf->SetFont('lato-regular');
+    $pdf->SetFontSize($kontaktFooterFontSize);
+    $pdf->SetTextColor(255,255,255); //weiss
+    $pdf->SetXY($kontaktFooterPositionX + $anschnitt, $kontaktFooterPositionY + $anschnitt);
+    $pdf->Cell($kontaktFooterWidth,0,$kontaktFusszeileText,0,0,'C');
+
+    // Rendern Community Name
+    echo "Verarbeite Community Name...\n";
+    $pdf->SetFont('alternategothic');
+    $pdf->SetFontSize($communityNameFontSize);
+    $pdf->SetTextColor(0,0,0); //schwarz
+    $pdf->SetXY($communityNamePositionX + $anschnitt, $communityNamePositionY + $anschnitt);
+    $pdf->MultiCell($wRechts, 10, $communityNameText,0,'C');
+
+    // Das war's mit dem Editieren
+
+
+    // Original PDF Rueckseit uebernehmen
+    $pdf->AddPage('L', array($dokumentBreite, $dokumentHoehe));
+    $tplRueckseite = $pdf->importPage(2);
+    $pdf->useTemplate($tplRueckseite);
+    
+    // und erstmal abspeichern
+    echo "Zwischenspeichern...\n";
+    $pdf->Output($outputFile);
+
+    // Hier geht jetzt der Hack wegen der Footerzeile weiter
+    // Die gerade abgespeicherte Datei wird erneut eingelesen
+    // um dann im Seiten-Format der Ursprungsdatei erneut abgespeichert zu werden.
+    // Is' doof, muss aber sein
+
+    $pdf_2 = new FPDI();
+    echo "Erneut laden...\n";
+    $pageCount = $pdf_2->setSourceFile($outputFile);
+    echo "Feinschliff...\n";
+    $Vorderseite_2 = $pdf_2->ImportPage(1);
+    $Rueckseite_2 = $pdf_2->ImportPage(2);
+    $pdf_2->AddPage('L', array($dokumentBreite, $dokumentHoehe));
+    $tplForderseite = $pdf_2->importPage(1);
+    $pdf_2->useTemplate($tplForderseite);
+    $pdf_2->AddPage('L', array($dokumentBreite, $dokumentHoehe));
+    $tplRueckseite = $pdf_2->importPage(2);
+    $pdf_2->useTemplate($tplRueckseite);
+
+    echo "Output: $outputFile\n";
+    $pdf_2->Output($outputFile);
+    unset ($pdf);
+    unset ($pdf_2);
+}
+
+?>
